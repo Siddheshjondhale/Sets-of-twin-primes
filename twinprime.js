0ca27538767fcce3ff55516f28cd97df393b9627
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2024-06-25 17:51:32 +0530

    Update twinprime.js

--- twinprime.js
+++ twinprime.js
@@ -1,3 +1,4 @@
+// To check Prime number
 function checkPrime(num, n) {
   for (let j = 2; j <= Math.sqrt(num); j++) {
     if (num != j && num % j == 0) {
@@ -8,6 +9,7 @@ function checkPrime(num, n) {
   return true;
 }
 
+// returns Number of pairs of two primes possible till n
 function NoOfTwinPrime(n) {
   let ctr = 0;
   let isPrime = true;
@@ -16,23 +18,17 @@ function NoOfTwinPrime(n) {
     return ctr;
   } else {
     for (let i = 2; i <= n; i++) {
-      // console.log(lastprime)
-
       isPrime = checkPrime(i, n);
-
       if (isPrime) {
         if (i - lastprime == 2) {
           //   console.log("I and LastPrime is ", i, lastprime);
           ctr++;
         }
-
         lastprime = i;
       }
-
       isPrime = true;
-    }
+    }    // end of loop
   }
-
   return ctr;
 }
 
